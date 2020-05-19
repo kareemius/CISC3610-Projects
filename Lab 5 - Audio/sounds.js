80cba02784f2dc6de1713d8cfa9510a2b863a0b9
kareem edmonds <kareeme4713@gmail.com>
2020-05-19 07:18:40 -0400

    1st Commit

---
+++ Lab 5 - Audio/sounds.js
@@ -0,0 +1,26 @@
+
+
+	function playStageAudio() {
+	var stage = document.getElementById('MM2 Stage Select');
+	stage.play();
+	}
+	
+	function playStartAudio() {
+	var start = document.getElementById('MM2 Level Start');
+	start.play();
+	}
+	
+	function playClearAudio() {
+	var clear = document.getElementById('MM2 Stage Clear');
+	clear.play();
+	}
+	
+	function playWilyAudio() {
+	var wily = document.getElementById('MM2 Wily Castle');
+	wily.play();
+	}
+	
+	function playGameOverAudio() {
+	var gameover = document.getElementById('MM2 Game Over');
+	gameover.play();
+	}
